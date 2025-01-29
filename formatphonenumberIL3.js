395b9a20d09daabcc1d8d3e8cbcdacd0c567bfe2
MosheCloudcom <133872774+MosheCloudcom@users.noreply.github.com>
2025-01-29 21:23:23 +0200

    Add files via upload

---
+++ formatphonenumberIL3.js
@@ -0,0 +1,23 @@
+function addCountryCode() {
+    var phoneField = document.getElementById('116683822'); // Original phone field
+    var formattedPhoneField = document.getElementById('116683856'); // Numeric phone field
+
+    var phoneValue = phoneField.value; // Get the phone field value
+    console.log("Original phone value:", phoneValue);
+
+    if (phoneValue) {
+        // Remove leading zero and add the 972 prefix
+        var formattedPhone = "972" + phoneValue.replace(/^0/, "");
+        console.log("Formatted phone:", formattedPhone);
+
+        // Update the numeric field with the formatted phone number
+        formattedPhoneField.value = formattedPhone;
+        console.log("Phone number updated in the numeric field:", formattedPhone);
+    } else {
+        console.log("No phone number found.");
+    }
+}
+
+
+
+window.onclick = addCountryCode;
