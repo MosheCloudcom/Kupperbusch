ee8189d2af20e482d7cf4a38a895a2d4f2e1540d
MosheCloudcom <133872774+MosheCloudcom@users.noreply.github.com>
2025-02-06 18:13:52 +0200

    Add files via upload

---
+++ createticketsubject2-1.js
@@ -0,0 +1,29 @@
+function CreateTicketSubject() {
+	
+	var BrandField = loader.engine.document.getElementById(116819571); // get the Brand Name Field
+	var BrandValue = BrandField.getProperty('value.value'); // get the Brand Name value
+    console.log("Brand value:", BrandValue);
+
+	var OrderNumberField = loader.engine.document.getElementById(116819252); // get the Order Number field
+	var OrderValue = OrderNumberField.getProperty('value.value'); // get the Order Number Value value
+    console.log("Brand value:", BrandValue);
+
+	var formattedTicketSubjectField = loader.engine.document.getElementById(116819573); // Get Formatted Ticket Subject Field
+
+    if (BrandField && OrderNumberField) {
+		
+		// Construct the formatted ticket subject
+		var formattedSubject = " הזמנה חדשה: " + OrderValue + " (מותג: " + BrandValue +")";
+		console.log("Formatted Ticket Subject:", formattedSubject);
+
+		// Set the value of the formatted ticket subject field		
+		formattedTicketSubjectField.setValue({ "value": formattedSubject });
+
+    } else {
+        console.log("No Brand or Order found");
+    }
+}
+
+
+
+window.onclick = CreateTicketSubject;
